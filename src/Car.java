b6f982e7e0814a8c34529572daae084439a99bb1
sleepyren <96728203+sleepyren@users.noreply.github.com>
2024-07-05 01:19:17 -0400

    implements Comparable

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,11 @@
-public class Car {
+import java.util.HashMap;
+
+public class Car implements Comparable<Car>{
     private final long REC_ID;
     private final String SERIAL;
     private final String DESTINATION;
     private COLOR COLORENUM;
+
     private enum COLOR {RED("Red"), BLUE("Blue"), BLACK("Black"), WHITE("White");
 
         private final String colorName;
@@ -25,6 +28,21 @@ public class Car {
 
     }
 
+    private static final HashMap<String, Integer> map = getDestinationMap();
+
+    public static HashMap<String, Integer> getDestinationMap() {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("New York", 0);
+        map.put("Miami", 1);
+        map.put("New Orleans", 2);
+        map.put("Houston", 3);
+        map.put("Los Angeles", 4);
+        return map;
+
+    }
+
+
     Car(long rec_id, String serial, String destination, int color) {
         this.REC_ID = rec_id;
         this.SERIAL = serial;
@@ -38,4 +56,18 @@ public class Car {
                 + '\t' + this.DESTINATION + '\t' + Long.toString(this.REC_ID) + '\n';
     }
 
+
+    @Override
+    public int compareTo(Car car) {
+        int compareDestination = map.get(this.DESTINATION) - map.get(car.DESTINATION);
+        if (compareDestination == 0) {
+            int compareColorEnum = this.COLORENUM.ordinal() - car.COLORENUM.ordinal();
+            if (compareColorEnum == 0) return this.SERIAL.compareTo(car.SERIAL);
+            else {
+                return compareColorEnum;
+            }
+        }
+        else
+            return compareDestination;
+    }
 }
