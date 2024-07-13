b5bbbffda558e0d45bdda6cb0444a89689cb5bb1
sleepyren <96728203+sleepyren@users.noreply.github.com>
2024-07-13 16:45:07 -0400

    object oriented sortjob design

---
+++ src/sortcarsv3/QuicksortEngine.java
@@ -0,0 +1,8 @@
+package sortcarsv3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuicksortEngine {
+    List<SortJob> jobs = new ArrayList<SortJob>();
+}
---
+++ src/sortcarsv3/SortJob.java
@@ -0,0 +1,79 @@
+package sortcarsv3;
+
+import sortcarsv3.QuicksortEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+import sortcarsv3.Car;
+
+import static sortcarsv3.Car.swap;
+
+
+public class SortJob {
+    public int startIndex;
+    public int endIndex;
+    public List<Car> list;
+    public int pivot;
+    private final QuicksortEngine engine;
+
+    //mainJob
+    public SortJob(List<Car> list, QuicksortEngine engine) {
+        this.startIndex = 0;
+        this.endIndex = list.size()-1;
+        this.list = list;
+        this.engine = engine;
+    }
+
+    public SortJob(int startIndex, int endIndex, SortJob parent) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        this.engine = parent.engine;
+        this.list = parent.list;
+    }
+
+    private void addChildJobs() {
+        SortJob LeftSort = new SortJob(this.startIndex, this.pivot-1, this);
+        SortJob RightSort = new SortJob(this.pivot+1, this.endIndex, this);
+
+        synchronized (this.engine)
+        {
+            this.engine.jobs.add(LeftSort);
+            this.engine.jobs.add(RightSort);
+        }
+    }
+
+    private void partition() {
+        this.pivot = this.startIndex;
+        int left = this.startIndex+1;
+        int right = this.endIndex;
+        while (left <= right) {
+
+            int comparison = this.list.get(left).compareTo(this.list.get(this.pivot));
+
+            if (comparison < 0)
+            {
+                left++;
+            }
+            else
+            {
+                swap(this.list, left, right);
+                right--;
+            }
+        }
+
+        swap(this.list, this.pivot, right);
+        this.pivot = right;
+
+    }
+
+    public void quickSort() {
+        if (this.startIndex < this.endIndex) {
+            this.partition();
+            this.addChildJobs();
+        }
+
+    }
+
+
+
+}
